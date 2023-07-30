66d4151346498fcb50512eb92913799fc06e157b
Alexei-T1 <turencko.alexei@yandex.ru>
2023-07-30 18:55:14 +0400

    add base module structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import './reset.css';
+import './style.css';
+
+import { mainStart } from './modules/startPage.js'
+
+/* css styles */
+const styleWrapper = "wrapper";
+
+const wrapper = document.createElement('div');
+wrapper.classList.add(styleWrapper);
+
+window.onload = () => {
+  const load = document.querySelector('.load');
+  load.replaceWith(mainStart);
+}
+
---
+++ src/modules/startPage.js
@@ -0,0 +1,13 @@
+
+/* css styles*/
+
+
+export const mainStart = document.createElement('main');
+
+const section1 = document.createElement('section');
+const section2 = document.createElement('section');
+section1.innerText = 'section1';
+section2.innerText = 'section12';
+
+mainStart.append(section1, section2);
+
