f2fa9355fec5a490b11136a8e976534e75a9dbf0
Alexei-T1 <turencko.alexei@yandex.ru>
2023-10-07 20:38:47 +0400

    add prototype header

--- src/modules/header.js
+++ src/modules/header.js
@@ -1,18 +1,63 @@
 /* css styles*/
+const mainText = "mainText";
+const headerClass = "headerSection";
+const logoClass = "logoHeader";
+const logoTextClass = "logoText";
+const navClass = "navHeader";
+const navTextClass = "navText";
+const navWelcomeClass = "navWelcome";
+const navDescribeClass = "navDescribe";
+const navContactClass = "navContact";
 
+/* images*/
+import imageBread from "../assets/sliced-brown.jpg";
 
 export const header = document.createElement('header');
-
+header.classList.add(headerClass);
 
 const logo = document.createElement('div');
-logo.textContent = "LOGO";
+logo.classList.add(logoClass);
+const imageLogo = document.createElement('img')
+imageLogo.src = imageBread;
+
+const textWrapLogo = document.createElement('div');
+textWrapLogo.classList.add(logoTextClass, mainText);
+const firstText = document.createElement('span');
+firstText.innerText = "Three";
+const secondText = document.createElement('span');
+secondText.innerText = "bread Crusts";
+const threeText = document.createElement('span');
+threeText.innerText = "restaurant";
+textWrapLogo.append(firstText, secondText, threeText);
+logo.append(imageLogo, textWrapLogo);
+
 
 const nav = document.createElement('nav');
+nav.classList.add(navClass);
 const ul = document.createElement('ul');
 nav.append(ul);
+
 const mainWrap = document.createElement('li');
+mainWrap.classList.add(navTextClass);
+const textMain = document.createElement('a');
+textMain.classList.add(navWelcomeClass);
+textMain.innerText = "Welcome";
+mainWrap.append(textMain);
+
 const describeWrap = document.createElement('li');
+describeWrap.classList.add(navTextClass);
+const textDescribe = document.createElement('a');
+textDescribe.classList.add(navDescribeClass);
+textDescribe.innerText = "Describe";
+describeWrap.append(textDescribe);
+
 const contactWrap = document.createElement('li');
+contactWrap.classList.add(navTextClass);
+const textContact = document.createElement('a');
+textContact.classList.add(navContactClass);
+textContact.innerText = "Contacts";
+contactWrap.append(textContact);
+
 ul.append(mainWrap, describeWrap, contactWrap);
 
 header.append(logo, nav);
